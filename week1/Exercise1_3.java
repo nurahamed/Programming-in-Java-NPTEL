4606c9c65995730b6a0d67f38d9ac44a58d322c8
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-01-29 11:37:13 +0530

    Exercise1_3

---
+++ week1/Exercise1_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+	  int sum=0;
+         //Use for or while loop do the operation.
+for(int i=0;i<=n;i++){
+			 int t = 2*(i-1);
+			if(t%3 == 0){
+				sum += t;
+			}
+		}
+		System.out.println(sum);
+       }
+}
